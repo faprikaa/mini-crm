3367da42297c74bca7757fd05bde8dda3116389c
faprikaa <93901661+faprikaa@users.noreply.github.com>
2026-02-15 11:51:39 +0700

    fix

--- src/app/promo-ideas/page.tsx
+++ src/app/promo-ideas/page.tsx
@@ -1,6 +1,8 @@
 import { getWeeklyPromoIdeas } from "@/lib/promo-ideas";
 import { PromoIdeasClient } from "./promo-ideas-client";
 
+export const dynamic = "force-dynamic";
+
 export default async function PromoIdeasPage() {
   const ideas = await getWeeklyPromoIdeas();
   return <PromoIdeasClient ideas={ideas} />;
